9d41711ca6326a75c8b123f64d5676bd63da6a85
GwenAQW <33049929+GwenAQW@users.noreply.github.com>
2017-10-23 23:25:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,18 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-	colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
-bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) { 
-	// Our bot needs to know if it will execute a command
-	// It will listen for messages that will start with 'Gwen, '
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
    if (message.substring(0,6) == 'Gwen, ') {
 		var args = message.substring(6).split(' ');
 		var cmd = args[0]
