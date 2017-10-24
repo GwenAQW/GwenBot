d32b25d8f2558b0a634773e4f0a21fd940666aa4
GwenAQW <33049929+GwenAQW@users.noreply.github.com>
2017-10-23 23:17:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,25 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+	colorize: true
 });
-
-client.on('message', message => {
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+	token: auth.token,
+	autorun: true
+});
+bot.on('ready', function (evt) {
+	logger.info('Connected');
+	logger.info('Logged in as: ');
+	logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) { 
+	// Our bot needs to know if it will execute a command
+	// It will listen for messages that will start with 'Gwen, '
    if (message.substring(0,6) == 'Gwen, ') {
 		var args = message.substring(6).split(' ');
 		var cmd = args[0]
